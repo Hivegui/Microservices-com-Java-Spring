53c532c792e815f6867b5dc3022dd0e562f58b37
Guilherme Gomes <93009014+Hivegui@users.noreply.github.com>
2024-03-14 11:39:35 -0300

    Add files via upload

---
+++ email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package com.ms.email.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${broker.queue.email.name}")
+    private String queue;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+}
---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,27 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
+import com.ms.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    final UserRepository userRepository;
+    final UserProducer userProducer;
+
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
+        this.userRepository = userRepository;
+        this.userProducer = userProducer;
+    }
+
+    @Transactional
+    public UserModel save(UserModel userModel){
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
+    }
+
+}
